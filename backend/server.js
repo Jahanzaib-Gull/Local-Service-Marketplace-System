c182b7d9c9f05fbdf9d8643c04a0e29511fe3615
Jahanzaib-Gull <zaibgull1423@gmail.com>
2026-03-10 07:17:01 +0500

    add backed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require('./app');
 
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("API Running");
-});
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
